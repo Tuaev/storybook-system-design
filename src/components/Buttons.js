15c7f353d64222a316d5cca7f96ed1fa3ccb3fa1
Soslan <sparksoslan@gmail.com>
2021-05-12 21:31:26 +0100

    refactor button variant handlers

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,47 +1,60 @@
 import styled from 'styled-components';
 import { defaultTheme, typeScale } from '../utils';
 
-const warningStyles = `
-  background: none;
-  color: ${defaultTheme.status.warningColor};
-  &:hover,
-  &:focus {
-    background-color: ${defaultTheme.status.warningColorHover};
-    outline: 3px solid ${defaultTheme.status.warningColorHover};
-    outline-offset: 2px;
-    border: 2px solid transparent;
-  }
+const buttonModifierStyles = {
+  small: () => `
+  font-size: ${typeScale.helperText};
+  padding: 8px;
+`,
+  large: () => `
+  font-size: ${typeScale.header5};
+  padding: 16px 24px;
+`,
+  warning: () => `
+    background: none;
+    color: ${defaultTheme.status.warningColor};
+    &:hover,
+    &:focus {
+      background-color: ${defaultTheme.status.warningColorHover};
+      outline: 3px solid ${defaultTheme.status.warningColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
 
-  &:active {
-    background-color: ${defaultTheme.status.warningColorActive};
-  }
-`;
-
-const primaryButtonWarning = `
-  background-color: ${defaultTheme.status.warningColor};
-  color: ${defaultTheme.textColorInverted};
-  `;
-
-const errorStyle = `
-  background: none;
-  color: ${defaultTheme.status.errorColor};
-  &:hover, &:focus {
-    background-color: ${defaultTheme.status.errorColorHover};
-    outline: 3px solid ${defaultTheme.status.errorColorHover};
-    outline-offset: 2px;
-    border: 2px solid transparent;
-  }
-  &:active {
-    background-color: ${defaultTheme.status.errorColorActive};
-  }
-`;
+    &:active {
+      background-color: ${defaultTheme.status.warningColorActive};
+    }`,
+  error: () => `
+    background: none;
+    color: ${defaultTheme.status.errorColor};
+    &:hover, &:focus {
+      background-color: ${defaultTheme.status.errorColorHover};
+      outline: 3px solid ${defaultTheme.status.errorColorHover};
+      outline-offset: 2px;
+      border: 2px solid transparent;
+    }
+    &:active {
+      background-color: ${defaultTheme.status.errorColorActive};
+    }`,
+};
 
 const handleVariant = (variant) => {
   switch (variant) {
     case 'warning':
-      return warningStyles;
+      return buttonModifierStyles.warning;
     case 'error':
-      return errorStyle + primaryButtonWarning;
+      return buttonModifierStyles.error;
+    default:
+      return;
+  }
+};
+
+const handleSize = (size) => {
+  switch (size) {
+    case 'small':
+      return buttonModifierStyles.small;
+    case 'large':
+      return buttonModifierStyles.large;
     default:
       return;
   }
@@ -49,12 +62,7 @@ const handleVariant = (variant) => {
 
 const Button = styled.button`
   padding: 12px 24px;
-  font-size: ${(props) =>
-    props.small
-      ? typeScale.helperText
-      : props.large
-      ? typeScale.header5
-      : typeScale.paragraph};
+  font-size: ${typeScale.paragraph};
   border-radius: 2px;
   min-width: 100px;
   cursor: pointer;
@@ -76,6 +84,7 @@ const Button = styled.button`
     border-color: ${defaultTheme.primaryColorActive};
     color: ${defaultTheme.textColorOnPrimary};
   }
+  ${({ size }) => handleSize(size)}
 `;
 
 export const PrimaryButton = styled(Button)`
