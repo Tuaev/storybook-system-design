e7c277b315bed7b06ec5747c48b918dd06d2204f
Soslan <sparksoslan@gmail.com>
2021-05-13 13:12:47 +0100

    Add darktheme and themeprovider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { useState } from 'react';
 import { PrimaryButton, SecondaryButton, TertiaryButton } from './components/Buttons';
+import { darkTheme, defaultTheme } from './utils';
+import { ThemeProvider } from 'styled-components';
 
 const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+
   return (
-    <div>
-      <PrimaryButton>Hello World</PrimaryButton>
-      <SecondaryButton disabled>Hello World</SecondaryButton>
-      <TertiaryButton disabled>Hello World</TertiaryButton>
-    </div>
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{ margin: '0 16px 24px', padding: '8px', background: 'none' }}
+        onClick={() => setUseDarkTheme(!useDarkTheme)}
+      >
+        Change Theme
+      </button>
+      <div
+        style={{
+          background: useDarkTheme ? defaultTheme.primaryColor : darkTheme.primaryColor,
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}
+      >
+        <PrimaryButton variant="warning" size="large">
+          Hello World
+        </PrimaryButton>
+        <SecondaryButton>Hello World</SecondaryButton>
+        <TertiaryButton>Hello World</TertiaryButton>
+      </div>
+    </ThemeProvider>
   );
 };
 
